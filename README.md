d1467855a77564f8b1c4bcfe779a30dce73bbf62
tsuji satoru <s-tsuji@clinks.jp>
2021-01-02 21:18:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## アプリケーション名
+GamingMarket
+
+## DB構造
+
+### users
+|Column|Type|Options|Index|
+|------|----|-------|-----|
+
+#### Association
+ - has_many :games
+
+#### games
+|Column|Type|Options|Index|
+|------|----|-------|-----|
+
+game_name|string
+price|integer
+genre|string
+user_id|integer
+game_item_id|integer
+#### Association
+ - belongs_to :user
+
+### cart_items
+|Column|Type|Options|Index|
+|------|----|-------|-----|
+quantity|integer
+user_id|integer
+game_id|integer
+
+#### Association
+ - belongs_to :user
+ - has_many :games
+
---
+++ database/migrations/2021_01_02_120608_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('game_name');
+            $table->string('genre');
+            $table->integer('price');
+            $table->integer('user_id');
+            $table->integer('game_item_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}

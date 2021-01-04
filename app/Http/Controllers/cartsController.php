89fbc507f3fa4fbd811b2f6ab2a80f8d9e2148a7
tsuji satoru <s-tsuji@clinks.jp>
2021-01-04 13:40:53 +0900

    cart controller Model view

---
+++ app/Http/Controllers/cartsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; 
+
+class cartsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Cart::updateOrCreate([
+            'user_id'=>Auth::id(),
+            'item_id'=>$request->post('item_id'),
+            'quantity'=>\DB::row('quantity+'.$request->post('quantity'))
+        ]);
+
+        return redirect()->route('game.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cart $cart)
+    {
+        //
+    }
+}
--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -7,6 +7,21 @@
         <div class="card">
             <div class="card-header"><a href="{{route('game.show',['id'=>$game->id])}}">{{$game->game_name}}</a></div>
             <div class="card-body">{{$game->price}}</div>
+            @auth 
+                <form action="{{route('cart.store')}}" method="post">
+                @csrf  
+                <input type="hidden" name="game_id" value="{{$game->id}}">
+                <select name="quantity">
+                    <option selected>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                </select>
+                <button type="submit">カートに入れる</button>
+                </form>
+            @endauth
         </div>
     @endforeach
     </div>
